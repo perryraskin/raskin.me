303f94ad28367f869c0ebc56a49b284f99fc7d2c
Perry Raskin <perryraskin@gmail.com>
2020-03-02 21:42:00 -0500

    Sitemap fix

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -7,7 +7,7 @@ const prettier = require('prettier');
     const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
 
     // Ignore Next.js specific files (e.g., _app.js) and API routes.
-    const pages = await globby(['pages/**/*{.js,.mdx}', '!pages/_*.js', '!pages/api']);
+    const pages = await globby(['posts/*.md', 'pages/**/*.tsx', '!pages/_*{.jsx,.tsx}']);
     const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -15,8 +15,9 @@ const prettier = require('prettier');
                 .map((page) => {
                     const path = page
                         .replace('pages', '')
-                        .replace('.js', '')
-                        .replace('.mdx', '');
+                        .replace('posts', '/blog')
+                        .replace('.tsx', '')
+                        .replace('.md', '');
                     const route = path === '/index' ? '' : path;
 
                     return `
