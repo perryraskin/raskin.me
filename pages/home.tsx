d8d2dea7c74ec2262af238c94c0bea339946df2f
Perry Raskin <perryraskin@gmail.com>
2020-03-06 14:56:21 -0500

    Styled home a bit better

--- pages/home.tsx
+++ pages/home.tsx
@@ -4,7 +4,7 @@ import Section from '../components/Section';
 import Button from '../components/Button';
 import Subscribe from '../components/Subscribe';
 
-import { Twitter, GitHub, Linkedin } from "react-feather";
+import { Twitter, GitHub, Linkedin, Mail } from "react-feather";
 
 import { data } from '../data.js';
 
@@ -24,12 +24,15 @@ const Home: NextPage<HomeProps> = ({
 }) => {
   return (
     <Layout>
-      <Section>
+      <Section extend="text-center lg:text-left">
       <p className="text-gray-600 dark:text-neutral-300 text-2xl md:text-4xl">
         Thanks for stopping by! 👋
       </p>
       <h2 className="text-4xl md:text-6xl leading-tight font-bold dark:text-neutral-10 mb-8 border-b-0">
-        I’m Perry Raskin. Full stacking. Side projecting. Learning.
+        I’m Perry Raskin.<br />
+        Full stacking.<br />
+        Side projecting.<br />
+        Learning.
       </h2>
       <Button
         text=""
@@ -48,6 +51,12 @@ const Home: NextPage<HomeProps> = ({
         icon={<Twitter />}
         to="https://twitter.com/perryraskin"
         extend="bg-blue-400 text-white"
+      />&nbsp;&nbsp;&nbsp;
+      <Button
+        text=""
+        icon={<Mail />}
+        to="mailto:perry@raskin.me"
+        extend="bg-gray-500 text-white"
       />
     </Section>
     <Section>
