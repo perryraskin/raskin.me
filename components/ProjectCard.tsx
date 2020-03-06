12238d4cff4a1b3e211293a4ce6749ffbfc8c439
Perry Raskin <perryraskin@gmail.com>
2020-03-06 13:44:23 -0500

    Implemented GitHub API to list repos on Projects page

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,49 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+interface ProjectCardProps {
+  repoName: string;
+  description: string;
+  url: string;
+  homepage: string;
+  language: string;
+  stars: string;
+  forks: string;
+}
+
+const ProjectCard: NextPage<ProjectCardProps> = ({
+  repoName,
+  description,
+  url,
+  homepage,
+  language,
+  stars,
+  forks
+}) => {
+  return (
+    <Link
+      href={url}
+    >
+      <a className="w-full px-2 my-2 overflow-hidden overflow-visible md:my-2 md:px-2 md:w-1/2 h-65" href={url}>
+        <article className="flex flex-col items-start justify-between h-full p-6 bg-white rounded-lg shadow dark:bg-neutral-700">
+          <div>
+            <h3 className="mt-0 text-3xl">{repoName}</h3>
+            <p className="text-xl leading-tight text-neutral-500 dark:text-neutral-200">
+              {description}
+            </p>
+          </div>
+          <div className="flex items-center justify-between w-full mt-2">
+            <button className="px-3 py-1 text-sm font-semibold uppercase rounded-lg bg-primary-50 text-primary-600 dark:bg-primary-500 dark:text-primary-50">
+              {language}
+            </button>
+            {/* <span className="my-auto text-sm font-semibold text-neutral-200">
+              <a href={homepage}>WEBSITE</a>
+            </span> */}
+          </div>
+        </article>
+      </a>
+    </Link>
+  );
+}
+
+export default ProjectCard;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,14 +1,29 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
+import fetch from 'isomorphic-unfetch';
+
 import Layout from '../components/Layout';
 import Section from '../components/Section';
-import Head from 'next/head';
+import ProjectCard from '../components/ProjectCard';
 
 interface ProjectsProps {
+  repos: Array<RepoDetails>;
+}
 
+interface RepoDetails {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  homepage: string;
+  language: string;
+  stars: string;
+  forks: string;
+  isForked: boolean;
 }
 
 const Projects: NextPage<ProjectsProps> = ({
-
+  repos
 }) => {
   return(
     <Layout>
@@ -22,8 +37,63 @@ const Projects: NextPage<ProjectsProps> = ({
         This page will soon list my projects in an aesthetically pleasing format. Stay tuned...
         </p>
       </Section>
+      <Section extend="mt-0">
+        <div className="flex flex-wrap -mx-2">
+          {repos.map((repo: RepoDetails) => {
+            const id = repo.id;
+            const name = repo.name;
+            const description = repo.description;
+            const url = repo.url;
+            const homepage = repo.homepage;
+            const language = repo.language;
+            const stars = repo.stars;
+            const forks = repo.forks;
+
+            return (
+              <ProjectCard
+                key={id}
+                repoName={name}
+                description={description}
+                url={url}
+                homepage={homepage}
+                language={language}
+                stars={stars}
+                forks={forks}
+              />
+            );
+          })}
+        </div>
+      </Section>
     </Layout>
   )
+}
+
+Projects.getInitialProps = async () => {
+  // ctx contains the query param
+  const res = await fetch('https://api.github.com/users/perryraskin/repos');
+  const data = await res.json();
+
+  console.log(`Show repos fetched. Count: ${data.length}`);
+
+  const repos = data.map((repo: any) => {
+    return {
+      id: repo.id,
+      name: repo.name,
+      description: repo.description,
+      url: repo.html_url,
+      homepage: repo.homepage,
+      language: repo.language,
+      stars: repo.stargazers_count,
+      forks: repo.forks_count,
+      isForked: repo.fork
+    }
+  })
+
+  const filteredRepos = repos.filter((repo: any) => !repo.isForked)
+
+  return {
+    repos: filteredRepos
+  };
 }
 
 export default Projects;
