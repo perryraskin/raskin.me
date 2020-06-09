a415115a7805508127dadea2eabd9811b457e0c2
Perry Raskin <perryraskin@gmail.com>
2020-06-09 11:15:45 -0400

    Update home.tsx

--- pages/home.tsx
+++ pages/home.tsx
@@ -37,7 +37,7 @@ const Home: NextPage<HomeProps> = ({
       <p className="text-3xl">
       Full stack development is what I do most. 
       Currently, I'm working on <a href="https://pointway.app">Pointway</a>. It's an all-in-one tool 
-      to keep track of credit card purchases and earned points. It will also help you effeciently plan
+      to keep track of credit card purchases and earned points. It will also help you efficiently plan
       your next trip using those points!
       </p><br/>
       <Button
